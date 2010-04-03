58a8eed6c115618f35ecf34589adc9a6f27f8ac6
Mike Redd <mikered@hotmail.com>
2010-04-03 13:52:12 -0700

    modified:   scripts/nzbmatrix-latest.sh

--- scripts/nzbmatrix-latest.sh
+++ scripts/nzbmatrix-latest.sh
@@ -26,6 +26,12 @@ else
     $ELINKS -dump -dump-width 600 "http://rss.nzbmatrix.com/rss.php?cat=${SEARCH}&userid=${USERID}&apikey=${APIKEY}" > $TMP/nzblatest.txt
     echo -ne "Searching in $SEARCH @ NZBMatrix..."
     echo ""
-   $CAT $TMP/nzblatest.txt | grep "Name" | awk '{print $3,$4,$5,$6,$7,$8,$9,$10,$11,"-",$1}'
+SPLAT=$($CAT $TMP/nzblatest.txt | grep "Name" | awk '{print $3,$4,$5,$6,$7,$8,$9,$10,$11,"-",$1}')
+if [ "$SPLAT" = "error:nothing_found" ]; then
+    echo "No Files Found.."
+else
+    echo "$SPLAT"
 fi
+fi
+
 exit 0
